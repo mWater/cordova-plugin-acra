21bde1f9f25bcfef3b8e994ff7fe225c277888b6
grassick <clayton@claytronics.org>
2015-03-08 23:49:13 -0400

    Removed package-dependent toast

--- MyApplication.java
+++ MyApplication.java
@@ -3,13 +3,10 @@ package co.mwater.acraplugin;
 import android.app.Application;
 import org.acra.*;
 import org.acra.annotation.*;
-import co.mwater.clientapp.R;
 
 @ReportsCrashes(formKey="",
 	formUri = "http://api.mwater.co/v3/acra_reports",
-	reportType = org.acra.sender.HttpSender.Type.JSON,
-	mode = ReportingInteractionMode.TOAST,
-	resToastText = R.string.crash_toast_text)
+	reportType = org.acra.sender.HttpSender.Type.JSON)
 public class MyApplication extends Application {
   @Override
   public void onCreate() {
